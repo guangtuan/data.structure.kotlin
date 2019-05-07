34f7a504823badddbd5bc4c7db9905387fce06d7
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-05-07 22:18:06 +0800

    remove unused swap function

--- src/main/kotlin/tech/igrant/dataStructure/sort/BubbleSort.kt
+++ src/main/kotlin/tech/igrant/dataStructure/sort/BubbleSort.kt
@@ -1,11 +1,5 @@
 package tech.igrant.dataStructure.sort
 
-fun swap(input: Array<Int>, left: Int, right: Int) {
-    val tmp = input[left]
-    input[left] = input[right]
-    input[right] = tmp
-}
-
 fun bubbleSort(input: Array<Int>) {
     for (i in 0 until input.size) {
         for (j in i until input.size) {
