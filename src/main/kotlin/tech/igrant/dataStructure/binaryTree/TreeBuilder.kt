5ce9baf1bf88ac7db4e1af3fa7ba7e6810cd38aa
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-06-19 12:34:14 +0800

    build binary tree from inOrder sequence and preOrder sequence

--- src/main/kotlin/tech/igrant/dataStructure/binaryTree/TreeBuilder.kt
+++ src/main/kotlin/tech/igrant/dataStructure/binaryTree/TreeBuilder.kt
@@ -14,8 +14,42 @@ fun subTree(sortedArray: Array<Int>, left: Int, right: Int): Node<Int>? {
     val middle = left + (right - left) / 2
     val rootValue = sortedArray[middle]
     return Node(
-            data = rootValue,
-            lc = subTree(sortedArray, left, middle - 1),
-            rc = subTree(sortedArray, middle + 1, right)
+        data = rootValue,
+        lc = subTree(sortedArray, left, middle - 1),
+        rc = subTree(sortedArray, middle + 1, right)
     )
+}
+
+fun buildFromPreOrderAndInOrder(preOrder: Array<Int>, inOrder: Array<Int>): BinaryTree<Int> {
+    if (preOrder.size != inOrder.size) {
+        return BinaryTree(null)
+    }
+
+    val loopUp = inOrder.foldIndexed(mutableMapOf<Int, Int>(), { idx, acc, i ->
+        acc[i] = idx
+        return@foldIndexed acc
+    })
+    fun buildNode(preOrder: Array<Int>, inOrder: Array<Int>, preStart: Int, preEnd: Int, inStart: Int, inEnd: Int): Node<Int>? {
+        if (preStart > preEnd || inStart > inEnd) {
+            return null
+        }
+        val rootValue = preOrder[preStart]
+        var rootIndexAtInOrder = -1
+        loopUp[rootValue]?.let {
+            rootIndexAtInOrder = it
+        }
+        if (rootIndexAtInOrder == -1) {
+            return null
+        }
+        val leftTreeSize = rootIndexAtInOrder - inStart
+        val rightTreeSize = inEnd - rootIndexAtInOrder
+        return Node(
+            data = rootValue,
+            lc = buildNode(preOrder, inOrder, preStart + 1, preStart + leftTreeSize, inStart, rootIndexAtInOrder - 1),
+            rc = buildNode(preOrder, inOrder, preEnd - rightTreeSize + 1, preEnd, rootIndexAtInOrder + 1, inEnd)
+        )
+    }
+
+    val node = buildNode(preOrder, inOrder, 0, preOrder.size - 1, 0, inOrder.size - 1)
+    return BinaryTree(node)
 }
